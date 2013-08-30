8ee55c997553c62812fa9443e14c339cb6b0e264
nader-dab <nader.dabour@abv.bg>
2013-08-30 18:03:58 +0300

    Added base controller

---
+++ Recipies/Recipies/Controllers/BaseApiController.cs
@@ -0,0 +1,26 @@
+﻿using Recipies.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Recipies.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        protected T PerformOperationAndHandleExceptions<T>(Func<T> operation)
+        {
+            try
+            {
+                return operation();
+            }
+            catch (Exception ex)
+            {
+                var errResponse = this.Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                throw new HttpResponseException(errResponse);
+            }
+        }
+    }
+}
