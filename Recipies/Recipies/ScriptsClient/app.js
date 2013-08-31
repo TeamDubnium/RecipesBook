fd29743b55d85a639c251944abab592b95eec1d8
Elena Kirilova <el_b_k@mail.bg>
2013-08-31 23:13:17 +0300

    addded files for js client

---
+++ Recipies/Recipies/ScriptsClient/app.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="_references.js" />
+
+
+require.config({
+    paths: {
+        jquery: "libs/jquery-2.0.3",
+        rsvp: "libs/rsvp.min",
+        httpRequester: "libs/http-requester",
+        dataProvider: "app/dataProvider",
+        "class": "libs/class",
+        cryptoJs: "libs/sha1",
+        underscore: "libs/underscore",
+        kendoWeb: "libs/kendo.web.min"
+
+    }
+});
+
+require(["jquery", "kendoWeb", "app/view-models", "app/views"], function ($) {
+
+    var layout = new kendo.Layout('#content');
+    var router = new kendo.Router();
+    var viewModelFactory = viewModels.get();
+    var viewFactory = views.get("ScriptsClient/partials/");
+    router.route("/", function () {
+            var homeViewHtml = "";
+            viewFactory.mainNavView()
+				.then(function (viewHtml) {
+				    homeViewHtml = viewHtml;
+				    return viewModelFactory.buildCategoriesViewModel();
+				})
+				.then(function (vm) {
+				    $("#main-nav").append(homeViewHtml);
+				    var view = new kendo.View("main-nav-view-template", { model: vm });
+				    layout.showIn("#main-nav", view);
+				});
+    });
+
+    router.route("/auth", function () {
+        viewFactory.loginForm()
+			.then(function (loginFormHtml) {
+			    var vm = viewModelFactory.buildLoginFormVM(function () {
+			        //debugger;
+			        router.navigate("/");
+			    });
+			    var view = new kendo.View(loginFormHtml, { model: vm });
+			    layout.showIn("#content", view);
+			});
+    });
+
+    $(function () {
+        layout.render($("#wrapper"));
+        router.start("/");
+    });
+});
---
+++ Recipies/Recipies/ScriptsClient/app/view-models.js
@@ -0,0 +1,62 @@
+﻿/// <reference path="data.js" />
+/// <reference path="../libs/kendo.web.min.js" />
+
+window.viewModels = (function () {
+	var displayName = "";
+	var ViewModels = Class.create({
+		init: function (persister) {
+			this.persister = persister;
+			return this;
+		},
+		currentUser: function () {
+			return displayName;
+		},
+		buildCategoriesViewModel: function () {
+		    var categories = [
+                { name: "Dinner" },
+                { name: "Main dish" },
+                { name: "Side dish" },
+                { name: "Snack" },
+
+		    ]
+		    console.log(categories.length);
+				//var carsDataSource = new kendo.data.DataSource(cars);
+				var categoriesViewModel = new kendo.observable({
+				    categories: categories
+				})
+				return categoriesViewModel;
+			
+		},
+		buildLoginFormVM: function (successCallback) {
+			var self = this;
+			var viewModel = new kendo.observable({
+				username: "DonchoMinkov",
+				password: "Minkov",
+				message: "Common!",
+				loginUser: function (e) {
+					return self.persister.users
+						.login(this.get("username"), this.get("password"))
+							.then(function (name) {
+								displayName = name;
+								successCallback();
+							});
+				},
+				registerUser: function (e) {
+					return self.persister.users
+						.register(this.get("username"), this.get("password"))
+							.then(function (name) {
+								displayName = name;
+								successCallback();
+							});
+				}
+			});
+			return viewModel;
+		}
+	});
+
+	return {
+		get: function () {
+			return new ViewModels(persisters.get("api/"))
+		}
+	}
+}());
